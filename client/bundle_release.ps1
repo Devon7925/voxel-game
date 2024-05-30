bea450b27fe9e4e379e45b1adc2c3d0696bea045
Devon7925 <devonrutledge03@gmail.com>
2024-05-29 23:18:30 -0600

    Planning changes

--- client/bundle_release.ps1
+++ client/bundle_release.ps1
@@ -4,4 +4,5 @@ Copy-Item -Path "target/release/voxel-game.exe" -Destination "voxel-game"
 Copy-Item -Path "assets" -Destination "voxel-game" -Recurse
 Copy-Item -Path "decks" -Destination "voxel-game" -Recurse
 Copy-Item -Path "settings.yaml" -Destination "voxel-game"
-Compress-Archive -Path voxel-game -DestinationPath voxel-game.zip
+Compress-Archive -Force -Path voxel-game -DestinationPath voxel-game.zip
+Remove-Item -Path voxel-game -Recurse
